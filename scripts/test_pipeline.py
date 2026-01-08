3697055f4669b593cb912530e4c1a4c34be4a6de
syedhasanabbaszaidi <syedhasanabbaszaidi@gmail.com>
2026-01-08 12:02:07 +0530

    Fix

--- scripts/test_pipeline.py
+++ scripts/test_pipeline.py
@@ -11,31 +11,19 @@ from app.core.pipeline import (
 if __name__ == "__main__":
     yolo_model = YOLO("yolo11s.pt")
 
-    geom = GeometryConfig(
-        id_class=0,
-        metadata_target_height=420,
-        correction_angle=0.0
-    )
-
-    prep = PreprocessConfig(
-        k_denoise=3,
-        thresh_block=13,
-        thresh_c=3
-    )
-
-    post = PostprocessConfig(
-        confidence=0.4
-    )
+    geom = GeometryConfig(id_class=0, metadata_target_height=420, correction_angle=10.0)
+    prep = PreprocessConfig(k_denoise=3, thresh_block=13, thresh_c=3)
+    post = PostprocessConfig(confidence=0.5)
 
     result = img_2_json(
-        yolo_model=yolo_model,
-        img_path="samples/sc.png",
+        yolo_model,
+        "samples/s2.jpeg",
         dest_path="output",
         geom_params=geom,
         prep_params=prep,
-        post_params=post,
-        save_process=True
+        post_params=post
     )
-
-    print("RESULT:")
+    print("RESULT for Image:")
     print(result)
+
+    
